1bf7dd33b854dc3b7923ce74a08d103f02ad00d8
Christian Fisher <christian.fisher@carleton.ca>
2019-11-07 16:37:24 -0500

    Added Arduino sending method, and Started Application UDP send method.

--- Server/src/server/ServerUDP.java
+++ Server/src/server/ServerUDP.java
@@ -14,14 +14,14 @@ import java.net.*;
  */
 public class ServerUDP {
     private final static int PACKETSIZE = 100;
-   InetAddress ParkingController;
-    DatagramSocket socket = null;
+   InetAddress ParkingControllerAddress, AppAddress;
+    DatagramSocket ParkingSocket = null;
     
     public ServerUDP(){
         try{
-            socket = new DatagramSocket();
-            ParkingController = InetAddress.getByName("localhost");
-
+            ParkingSocket = new DatagramSocket();
+            ParkingControllerAddress = InetAddress.getByName("localhost");
+            AppAddress = InetAddress.getByName("localhost");
             
         }catch(Exception e){
             System.out.println(e);
@@ -40,18 +40,72 @@ public class ServerUDP {
         byte[] byteArray = data.getBytes();
         try{
         DatagramPacket ack = new DatagramPacket(new byte[PACKETSIZE], PACKETSIZE);
-        DatagramPacket packet = new DatagramPacket(byteArray, byteArray.length,ParkingController, 1001);
-        socket.send(packet);
-        socket.receive(ack);
+        DatagramPacket packet = new DatagramPacket(byteArray, byteArray.length,ParkingControllerAddress, 1001);
+        ParkingSocket.send(packet);
+        ParkingSocket.receive(ack);
         String messAck = new String(ack.getData()).trim();
         if("LEDack".equals(messAck)){
-            System.out.println("LED sucessfully toggled");
+            System.out.println("LED sucessfully Accessed");
         }else{
-            System.out.println("LED toggling FAILED");
+            System.out.println("LED Access FAILED");
         }
         }catch(Exception e){
             System.out.println("LED send failed");
         }
     }
+    public void sendToArduino(Boolean PinCorrect){
+        String data = "Arduino:";
+        data+= PinCorrect.toString();
+        byte[] byteArray = data.getBytes();
+        try{
+        DatagramPacket ack = new DatagramPacket(new byte[PACKETSIZE], PACKETSIZE);
+        DatagramPacket packet = new DatagramPacket(byteArray, byteArray.length,ParkingControllerAddress, 1001);
+        ParkingSocket.send(packet);
+        ParkingSocket.receive(ack);
+        String messAck = new String(ack.getData()).trim();
+        if("Arduinoack".equals(messAck)){
+            System.out.println("Arduino Received packet");
+        }else{
+            System.out.println("Arduino format error (Nack)");
+        }
+        }catch(Exception e){
+            System.out.println("Arduino send failed");
+        }
+    }
+    
+    public void sendToApp(){
+         String data = "Arduino:";
+        
+        byte[] byteArray = data.getBytes();
+        try{
+        DatagramPacket ack = new DatagramPacket(new byte[PACKETSIZE], PACKETSIZE);
+        DatagramPacket packet = new DatagramPacket(byteArray, byteArray.length,AppAddress, 1001);
+        ParkingSocket.send(packet);
+        ParkingSocket.receive(ack);
+        String messAck = new String(ack.getData()).trim();
+        if("Arduinoack".equals(messAck)){
+            System.out.println("Arduino Received packet");
+        }else{
+            System.out.println("Arduino format error (Nack)");
+        }
+        }catch(Exception e){
+            System.out.println("Arduino send failed");
+        }
+        
+    }
+    
+    public static void main(String[] args){
+        Spot pSpot[] = new Spot[9];
+        for(int i =0; i<9; i++){
+            pSpot[i]=new Spot();
+            pSpot[i].setLabel(Integer.toString(i+1));
+            System.out.println(pSpot[i].getLabel());
+        }
+        ServerUDP udp = new ServerUDP();
+        udp.sendToLED(pSpot[1]);
+        udp.sendToArduino(true);
+        
+    }
+    
     
 }
