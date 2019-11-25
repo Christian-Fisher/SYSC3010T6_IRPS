cb4c88b1a99911f0e94e500cd874e47cc35241a2
Christian Fisher <christian.fisher@carleton.ca>
2019-11-25 18:07:16 -0500

    Folder for final project

--- MyApplication2/app/src/main/java/com/example/myapplication2/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         appMainThread thread = new appMainThread();
-        thread.run();
+        thread.start();
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
