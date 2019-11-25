839ddcc499cd9a1f6dbd72c8011e92ce111be252
Christian Fisher <christian.fisher@carleton.ca>
2019-11-25 17:44:55 -0500

    added appmain to thread

--- MyApplication2/app/src/main/java/com/example/myapplication2/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -15,11 +15,14 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import java.io.IOException;
 import java.net.*;
+import java.io.IOException;
 import java.net.DatagramPacket;
-
-import static java.sql.DriverManager.println;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Stack;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,22 +38,27 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        // byte[] sendData= new byte [1024];
-        //  DatagramPacket sendPacket = new DatagramPacket(sendData,sendData.length); //udp packet
+        appMainThread thread = new appMainThread();
+        thread.run();
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
+
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        fab.setOnClickListener(
+                new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view
+            ) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        }
+        );
 
         mTextUsername = (EditText) findViewById(R.id.edittext_username);  // adding the id to the text space so we can use it in our methods
         mTextpassword = (EditText) findViewById(R.id.edittext_password);// adding the id to the text space so we can use it in our methods
@@ -75,17 +83,108 @@ public class MainActivity extends AppCompatActivity {
         // }
         // );
 
-        mButtonLogin.setOnClickListener(new View.OnClickListener() {
+        mButtonLogin.setOnClickListener(
+                new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if (verifyLogin(mTextUsername.getText().toString().trim(),mTextpassword.getText().toString().trim()) == true) {
+            public void onClick(View view
+            ) {
+                if (verifyLogin(mTextUsername.getText().toString().trim(), mTextpassword.getText().toString().trim()) == true) {
                     Intent LoginIntent = new Intent(MainActivity.this, book.class);
                     startActivity(LoginIntent);
                 } else {
                     Toast.makeText(MainActivity.this, "Login Error", Toast.LENGTH_SHORT).show(); // if not give the user an error msg
                 }
             }
-        });
+        }
+        );
+    }
+
+    class appMainThread extends Thread {
+
+        private final static String COMMAND_SPLIT_REGEX = ":";
+        private final static String DATA_SPLIT_REGEX = ",";
+        private final static String LED_COMMAND = "LED";
+        private final static String NOTHING_TO_REPORT = "NA";
+        private final static String OCCUPANCY_UPDATE_COMMAND = "OCC";
+        private final static String LOGIN_COMMAND = "LOG";
+        private final static String CLAIM_COMMAND = "CLA";
+
+        private final static int PACKETSIZE = 100;
+        DatagramSocket socket, sendSocket;
+        DatagramPacket packet;
+        InetAddress ServerAddress, local;
+        Stack<String> appQueue;
+
+        @Override
+        public void run() {
+            //Test
+            setup();
+
+            while (true) {
+                try {
+                    receive();
+
+                } catch (IOException e) {
+
+                }
+            }
+
+        }
+
+        public void setup() {
+            try {
+                appQueue = new Stack<>();
+                ServerAddress = InetAddress.getByName("localhost");
+                local = InetAddress.getByName("localhost");
+                socket = new DatagramSocket(2000);
+                socket.setSoTimeout(10000);
+                sendSocket = new DatagramSocket();
+
+            } catch (SocketException | UnknownHostException e) {
+                System.out.println("socket bad");
+            }
+
+        }
+
+        public void receive() throws IOException {
+
+            DatagramPacket heartbeat = new DatagramPacket(new byte[PACKETSIZE], PACKETSIZE);
+            socket.receive(heartbeat);
+            if (new String(heartbeat.getData()).trim().equals("HBAPP")) {
+                if (appQueue.isEmpty()) {
+                    DatagramPacket heartbeatAck = new DatagramPacket(NOTHING_TO_REPORT.getBytes(), NOTHING_TO_REPORT.getBytes().length, ServerAddress, 1000);
+                    sendSocket.send(heartbeatAck);
+                } else {
+                    String heartbeatRespond = appQueue.peek();
+                    DatagramPacket heartbeatAck = new DatagramPacket(heartbeatRespond.getBytes(), heartbeatRespond.getBytes().length, ServerAddress, 1000);
+                    sendSocket.send(heartbeatAck);
+
+                    if (heartbeatRespond.split(COMMAND_SPLIT_REGEX)[0].equals(LOGIN_COMMAND)) {
+                        DatagramPacket login = new DatagramPacket(new byte[PACKETSIZE], PACKETSIZE);
+                        socket.receive(login);
+                        sendSocket.send(new DatagramPacket((LOGIN_COMMAND + "ACK").getBytes(), (LOGIN_COMMAND + "ACK").getBytes().length, ServerAddress, 1000));
+
+                    } else if (heartbeatRespond.split(COMMAND_SPLIT_REGEX)[0].equals(OCCUPANCY_UPDATE_COMMAND)) {
+                        DatagramPacket lotOccupancyPacket = new DatagramPacket(new byte[PACKETSIZE], PACKETSIZE);
+                        socket.receive(lotOccupancyPacket);
+                        sendSocket.send(new DatagramPacket((OCCUPANCY_UPDATE_COMMAND + "ACK").getBytes(), (OCCUPANCY_UPDATE_COMMAND + "ACK").getBytes().length, ServerAddress, 1000));
+
+                    } else if (heartbeatRespond.split(COMMAND_SPLIT_REGEX)[0].equals(CLAIM_COMMAND)) {
+                        DatagramPacket ClaimPacket = new DatagramPacket(new byte[PACKETSIZE], PACKETSIZE);
+                        socket.receive(ClaimPacket);
+                        sendSocket.send(new DatagramPacket((CLAIM_COMMAND + "ACK").getBytes(), (CLAIM_COMMAND + "ACK").getBytes().length, ServerAddress, 1000));
+
+                    }
+                }
+            } else if (new String(heartbeat.getData()).trim().equals(LOGIN_COMMAND)) {
+                String loginRequest[] = new String(heartbeat.getData()).trim().split(COMMAND_SPLIT_REGEX);
+                appQueue.add(loginRequest[1]);
+                System.out.println("Succesfully added to queue");
+                sendSocket.send(new DatagramPacket((LOGIN_COMMAND + "ACK").getBytes(), (LOGIN_COMMAND + "ACK").length(), local, 3000));
+            }
+
+        }
+
     }
 
     public boolean verifyLogin(String username, String password) {
--- MyApplication2/app/src/main/java/com/example/myapplication2/claim.java
+++ MyApplication2/app/src/main/java/com/example/myapplication2/claim.java
@@ -7,42 +7,66 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.content.Intent;
-public class claim extends AppCompatActivity {
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketTimeoutException;
 
+public class claim extends AppCompatActivity {
 
     EditText mTextlicencesnumber;
     Button mButtonsubmitclaim2;
 
+    InetAddress local;
+    private final static String OCCUPANCY_UPDATE_COMMAND = "OCC";
+    private final static String LOGIN_COMMAND = "LOG";
+    private final static String CLAIM_COMMAND = "CLA";
+    private final static String COMMAND_SPLIT_REGEX = ":";
+    private final static String DATA_SPLIT_REGEX = ",";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_claim);
 
+        mTextlicencesnumber = (EditText) findViewById(R.id.edittext_platenumber);
+        mButtonsubmitclaim2 = (Button) findViewById(R.id.button_submitclaim2);
 
-        mTextlicencesnumber  = (EditText) findViewById(R.id.edittext_platenumber);
-        mButtonsubmitclaim2 = (Button)findViewById(R.id.button_submitclaim2);
-
-
-      // save the plate that the user inputed and send it to database to get checked
-
+        // save the plate that the user inputed and send it to database to get checked
         // get an avilable spot  from database and output it to the user
+        mButtonsubmitclaim2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                Intent LoginIntent = new Intent(claim.this, finalpage.class);
 
-
-
-         mButtonsubmitclaim2.setOnClickListener(new View.OnClickListener() {
-                      @Override
-                       public void onClick(View view) {
-                           Intent LoginIntent = new Intent(claim.this,finalpage.class);
-
-                           startActivity(LoginIntent);
-                      }
-                    });
-
-
-
-
+                startActivity(LoginIntent);
+            }
+        });
 
     }
+
+    public boolean verifyClaim(String licence) {
+        try {
+            DatagramSocket socket = new DatagramSocket(3000);
+            socket.setSoTimeout(10000);
+            DatagramSocket sendSocket = new DatagramSocket();
+
+            local = InetAddress.getByName("localhost");
+            String dataToSend = CLAIM_COMMAND + COMMAND_SPLIT_REGEX + licence;
+            DatagramPacket claimPacket = new DatagramPacket(dataToSend.getBytes(), dataToSend.getBytes().length, local, 2000);
+            DatagramPacket claimAck = new DatagramPacket(new byte[100], 100);
+
+            sendSocket.send(claimPacket);
+            socket.receive(claimAck);
+
+        } catch (SocketTimeoutException ex) {
+            System.err.println("Bad things happening");
+        } catch (IOException e) {
+            System.err.println(e + "heartbeat app failed");
+        }
+        return false;
+    }
+
 }
