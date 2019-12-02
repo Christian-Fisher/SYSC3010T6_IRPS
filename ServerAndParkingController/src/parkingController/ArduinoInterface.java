9fc2220144f186227e9d549e36a4e0ccac2d4f23
Christian Fisher <christian.fisher@carleton.ca>
2019-12-02 18:07:55 -0500

    Finished arduino interface

--- ServerAndParkingController/src/parkingController/ArduinoInterface.java
+++ ServerAndParkingController/src/parkingController/ArduinoInterface.java
@@ -6,6 +6,10 @@
 package parkingController;
 
 import com.fazecast.jSerialComm.*;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.util.Arrays;
 
 /**
@@ -15,21 +19,33 @@ import java.util.Arrays;
 public class ArduinoInterface {
 
     public static void main(String[] args) {
-
-        SerialPort port = SerialPort.getCommPort("ENTER PORT NAME HERE");
-        port.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING | SerialPort.TIMEOUT_WRITE_BLOCKING, 200, 200);
-        port.setBaudRate(9600);
-
         try {
+            SerialPort port = SerialPort.getCommPort("ENTER PORT NAME HERE");
+            port.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING | SerialPort.TIMEOUT_WRITE_BLOCKING, 2000, 2000);
+            port.setBaudRate(9600);
+            DatagramSocket socket = new DatagramSocket(3003);
+            DatagramSocket sendSocket = new DatagramSocket();
+            InetAddress Server = InetAddress.getByName("localhost");
+
             while (true) {
+
                 byte[] readBuffer = new byte[1024];
                 port.readBytes(readBuffer, readBuffer.length);
                 System.out.println(Arrays.toString(readBuffer));
+                if (!new String(readBuffer).equals("")) {
+                    DatagramPacket pinAck = new DatagramPacket(new byte[100], 100);
+                    sendSocket.send(new DatagramPacket(("ARD:" + new String(readBuffer)).getBytes(), ("ARD:" + new String(readBuffer)).getBytes().length, Server, 2000));
+                    socket.receive(pinAck);
+                    if (new String(pinAck.getData()).trim().split(":")[1].equals("true")) {
+                        port.writeBytes("1".getBytes(), "1".getBytes().length);
+                    }else{
+                        port.writeBytes("0".getBytes(), "0".getBytes().length);
+                    }
+                }
             }
-        }catch(Exception e){
+        } catch (SerialPortInvalidPortException | IOException e) {
             System.err.println(e);
         }
-            
 
     }
 }
