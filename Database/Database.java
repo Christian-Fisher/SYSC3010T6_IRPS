5a77142572fee6b4740786b6d52773eee5a93d2c
AkashJoe500 <47267360+AkashJoe500@users.noreply.github.com>
2019-12-02 15:47:21 -0500

    pins and names

--- Database/Database.java
+++ Database/Database.java
@@ -5,9 +5,15 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Database {
+	
+	int PIN1;
+	String[] userNames;
+	
 	private Connection connect() {
 		String url = "jdbc:sqlite:C:\\Users\\Akash\\Desktop\\Important\\Fall 2019\\SYSC 3010\\SYSC3010T6_IRPS\\SYSC3010T6_IRPS\\Database\\IRPSDatabase.db";
 		Connection connect = null;
+		//PIN1 = new Integer[10];
+		userNames = new String[10];
 		try {
             connect = DriverManager.getConnection(url);
         } catch (SQLException e) {
@@ -16,7 +22,7 @@ public class Database {
         return connect;
 	}
 	
-	public void selectAll(){
+	public void printAll(){
         String sql = "SELECT Username, PIN, LicensePlate, BookedSpot FROM Users";
         
         try (Connection conn = this.connect();
@@ -35,9 +41,45 @@ public class Database {
         }
     }
 	
+	public Integer getPINs() {
+		String sql = "SELECT Username, PIN, LicensePlate, BookedSpot FROM Users";
+		try(Connection conn = this.connect();
+	        Statement stmt  = conn.createStatement();
+	        ResultSet rs    = stmt.executeQuery(sql)){
+			
+			while(rs.next()) {
+				PIN1 = rs.getInt("PIN");
+				System.out.println(PIN1);
+			}
+		}catch (SQLException e) {
+            System.out.println(e.getMessage());
+	   }
+		return PIN1;
+   }
+	
+	public String[] getNames() {
+		int i=0;
+		String sql = "SELECT Username, PIN, LicensePlate, BookedSpot FROM Users";
+		try(Connection conn = this.connect();
+	        Statement stmt  = conn.createStatement();
+	        ResultSet rs    = stmt.executeQuery(sql)) {
+			while(rs.next()) {
+				userNames[i] = rs.getString("Username");
+				System.out.println("The usernames present in the database are "+userNames[i]);
+				i++;
+			}
+			
+		}catch (SQLException e) {
+            System.out.println(e.getMessage());
+		}
+		return userNames;
+	}
+
 	public static void main(String[] args) {
         Database db = new Database();
-        db.selectAll();
+        db.printAll();
+        db.getPINs();
+        db.getNames();
     }
 
 }
