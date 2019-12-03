4c6534708f03bf468201795295eeb26eb07b1874
Christian-Fisher <christianfisher@cmail.carleton.ca>
2019-12-03 18:24:38 -0500

    Added comments to guide you in making lot methods

--- Database/LotDB.java
+++ Database/LotDB.java
@@ -34,7 +34,30 @@ public class LotDB {
             System.out.println(e.getMessage());
         }
     }
-	
+        /*
+        public String[] getLotOccupancy()
+        
+        RETURNS: an array of strings. Each string contains a single spot's occupancy
+        */
+        
+        /*
+        public void carArrived(String spot, boolean occupancy)
+        This method takes a spot number in, and changes it's occupancy to true. This is different from bookSpot because this is for a user parking without booking.
+        */
+        
+	/*
+        public boolean bookSpot(String spot, String User)
+        Parameters: spot contains the spot that User is booking. 
+        Sets BookTime of the spot to the current time.
+        Set the User's BookedSpot to the spot number, set the spot's occupancy to true (call your carArrived method)
+        Returns: boolean that is true if the operation worked, false if there was a problem (spot not available ect)
+        */
+        /*
+        public boolean bookingTimeOut(String spot, float timeBooked)
+        Parameters: timeBooked is when the user booked their spot, spot is the spot number (this spot COULD not have a time booked (=null)
+        Checks if currentTime - timeBooked >=10 minutes
+        Returns boolean if the spot should be unbooked (set occupancy of spot to 0)
+        */
 	public void main(String args[]) {
 		LotDB db = new LotDB();
 		db.printAll();
