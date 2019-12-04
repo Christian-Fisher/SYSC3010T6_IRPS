dbbbf6093b69e1c3f0934a0b5008cd0b98d62b95
Christian Fisher <christian.fisher@carleton.ca>
2019-12-04 16:45:58 -0500

    Precompiling arduino code

--- ServerAndParkingController/src/ArduinoReceiver/ArduinoInterface.java
+++ ServerAndParkingController/src/ArduinoReceiver/ArduinoInterface.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package parkingController;
+package ArduinoReceiver;
 
 import com.fazecast.jSerialComm.*;
 import java.io.IOException;
