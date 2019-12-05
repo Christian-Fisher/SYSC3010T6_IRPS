14e57dd8cc0db7d9840ba14ccf23f7a1cff63670
Christian Fisher <christian.fisher@carleton.ca>
2019-12-04 21:18:42 -0500

    Rewrote database method

--- ServerAndParkingController/src/server/Database.java
+++ ServerAndParkingController/src/server/Database.java
@@ -470,7 +470,7 @@ public class Database {
         changeOccupancy(spot, true);
         setTime(spot);
         
-        String sql= "UPDATE Users SET  BookedSpot = '" + spot+ "' WHERE Users = '" + User + "';";
+        String sql= "UPDATE Username SET  BookedSpot = '" + spot+ "' WHERE Users = '" + User + "';";
         try {
             Connection conn = this.connect();
             Statement stmt = conn.createStatement();
