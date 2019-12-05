04c62142dd0753aae816756ea0c09de3d05d6fd3
Christian Fisher <christian.fisher@carleton.ca>
2019-12-05 10:48:40 -0500

    Fixed book spot LED bug. Serial and end case bugs left

--- ServerAndParkingController/src/server/ServerUDP.java
+++ ServerAndParkingController/src/server/ServerUDP.java
@@ -116,7 +116,7 @@ public class ServerUDP {
         try {
             DatagramPacket LoginAck = new DatagramPacket(data.getBytes(), data.getBytes().length, ParkingControllerAddress, 2001);
             sendSocket.send(LoginAck);
-            System.out.println("IR CALL: " + IRMessage[0] + " : " + IRMessage[1].equals("1"));
+            System.out.println("IR CALL: " + Integer.toString(Integer.parseInt(IRMessage[0]+1)) + " : " + IRMessage[1].equals("1"));
             d.changeOccupancy(Integer.toString(Integer.parseInt(IRMessage[0]+1)), IRMessage[1].equals("0"));
             sendToLED(IRMessage[0], IRMessage[1].equals("1"));
             if(IRMessage[1].equals("0")){
