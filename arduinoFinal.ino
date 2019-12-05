0cf4ba1a90dafb27add7fae21e3ee9af59524c0d
Shan-SYSC3010 <46901126+215shanram@users.noreply.github.com>
2019-12-04 23:56:31 -0500

    Finalized&testedOK

--- arduinoFinal.ino
+++ arduinoFinal.ino
@@ -1,99 +1,125 @@
+#include <Keypad.h>
 #include <Wire.h>
 // Include the Servo Motor library
 #include <Servo.h>
 //Iclude the Keypad library
-#include <Keypad.h>
-//Iclude the LCD library
-Servo servoMotor;
-int IRsensor1 = 0;
-int ledPin = 11; // same as mentioned below
-int signalPin = 12;// need to edit based on the connection for pin code
+Servo servoExit, servoEntry;
+int IRsensor = 13;
 int PIN_CodeLength = 5;
-float sensorRightValue;
-float sensorLeftValue;
-boolean range;
 int rotate;
-int pin = 0;
-int val =0;
+int pin = HIGH;
 const int buzzer = 10; //buzzer to arduino pin 10
-//Keypad
+boolean checkingPin = false;
+boolean pinOK = false;
 
 char pinReceive[4];
-char pinSet[] = "1234";
+String correctPin = "1234";
+String pinEntered = "";
 byte pinReceive_count = 0;
 byte pinSet_count = 0;
 bool checkPin; //checks for correct pin
 char entryKey; // user
 const byte ROWS = 4;
 const byte COLUMNS = 4;
+char pinSet[] = "1234";
 
 char keysPad[ROWS][COLUMNS] = {
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
+  {
+    '1', '2', '3', 'A'  }
+  ,
+  {
+    '4', '5', '6', 'B'  }
+  ,
+  {
+    '7', '8', '9', 'C'  }
+  ,
+  {
+    '*', '0', '#', 'D'  }
 };
 
-byte pins1[ROWS] = {9, 8, 7, 6};
-byte pins2[COLUMNS] = {5, 4, 3, 2};
+byte pins1[ROWS] = {
+  9, 8, 7, 6};
+byte pins2[COLUMNS] = {
+  5, 4, 3, 2};
 
 Keypad pinKeys = Keypad(makeKeymap(keysPad), pins1, pins2, ROWS, COLUMNS);
 
 void setup() {
   Serial.begin(9600);        //Serial connection initial setup at 9600 bits of data per second
   //pinMode(11, OUTPUT);       //setting the pin 13 in the arduino board as output pin
-  servoMotor.attach(11);      // attaching the servo motor function to pin 9
-  servoMotor.write(0);       // intial set up of the position of the servo motor which is now at 0 degrees
+  servoEntry.attach(12);      // attaching the servo motor function to pin 9
+  servoEntry.write(100);       // intial set up of the position of the servo motor which is now at 0 degrees
+  servoExit.attach(11);      // attaching the servo motor function to pin 9
+  servoExit.write(0);
   pinKeys.begin(makeKeymap(keysPad));
-  pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
-  pinMode(IRsensor1,INPUT);  //setting the first IR sensor as input
+  pinMode(buzzer, OUTPUT); // Set buzzer - pin 10 as an output
+  //pinMode(ledPin, OUTPUT); // Set buzzer - pin 12 as an output
+  pinMode(IRsensor,INPUT);  //setting the first IR sensor as input
 
 }
 
 void loop() {
-  open();
   entry();
-  close();
-  exit();
-  
+  //buzz(false);
+  //buzz(true);
+  exitGate();
+
 }
+
+
+void buzz(boolean isValid){
+
+  if (isValid == false){
+    tone(buzzer, 2000); // Send 1KHz sound signal...
+    delay(1000);  
+    noTone(buzzer);     // Stop sound...
+    //delay(100);        // ...for 1sec
+  }
+  if (isValid == true){
+    tone(buzzer, 4000); // Send 1KHz sound signal...
+    delay(100);        // ...for 1 sec
+    noTone(buzzer);    // Stop sound...
+    tone(buzzer, 4000);
+
+  }
+    else {
+    noTone(buzzer);     // Stop sound...
+  }
+}
+
+
 void entry() {
   char entryKey = pinKeys.getKey();
   if (entryKey) {
-    Serial.print(entryKey);
     pinReceive[pinReceive_count] = entryKey;
     pinReceive_count++;
   }
 
-  void exit(){
-    {
-    val = digitalRead(IRsensor1);     // read the input pin
-    //Serial.print(val);
-    //digitalWrite(ledPin, val);    // sets the LED to the button's value
-    if ( val ==1){
-      open();
-      delay(2000);
-      close();      
-    }
-}
-    
-  }
-  
   if (pinReceive_count == PIN_CodeLength - 1) {
     Serial.println(pinReceive);
-    if (strcmp(pinReceive, pinSet)) {
-    //open();
-    //digitalWrite(11, HIGH);
-    buzz(true);
-    delay(5000);
-    //close();
-    //digitalWrite(11, LOW);
-  } else {
-    buzz(false);
-    delay(2000);
+
+    //if (Serial.available()>=1) {
+//      String response = Serial.readString();
+      for(int i =0;i<=4; i++){
+      pinEntered += pinReceive[i];
+      }
+      if(strcmp(pinReceive,pinSet)){    //response.equals("1")
+        buzz(true);
+        delay(1000);
+        buzz('1');
+        entryGate(true);
+        delay(1000);
+        }
+      else{
+         buzz(false);
+        entryGate(false);
+        delay(2000);
+        buzz('1');
+      }
+    
+    clearData();
   }
-  clearData();
-  }
+  
 }
 void clearData() {
   while (pinReceive_count != 0) {
@@ -101,37 +127,45 @@ void clearData() {
   }
   return;
 }
-void open() {
-  for (rotate = 0; rotate <= 100; rotate += 1) { //rotating the motor to 90 degrees signifies the open gate
-    servoMotor.write(rotate);
+
+void exitGate(){
+  pin = digitalRead(IRsensor);
+  if (pin == LOW){
+    for (rotate = 0; rotate <= 100; rotate += 1) { //rotating the motor to 100 degrees to open gate
+    servoExit.write(rotate);
     delay(15);
+    }
+    delay(5000);
+    for (rotate = 100; rotate >= 1; rotate -= 1) { // closing gate after a delay
+    servoExit.write(rotate);
+    delay(15);
+    }
+    delay(1000);
+    }
+    
+    else{
+    }
   }
-  delay(1000);
+     
+
+void entryGate(boolean pinOK) {
+  if (pinOK==false){
+    for (rotate = 100; rotate >= 0; rotate -= 1) { //rotating the motor to 100 degrees to open gate
+    servoEntry.write(rotate);
+    delay(15);
+    }
+    delay(5000);
+    for (rotate = 0; rotate <= 100; rotate += 1) { // close gate after a delay
+    servoEntry.write(rotate);
+    delay(15);
+    }
+    delay(5000);
+    }
+    else{
+    }
+      
 }
 
-void buzz(bool isValid){
-  
-  if (isValid = true){
-    tone(buzzer, 1000); // Send 1KHz sound signal...
-    delay(100);        // ...for 1 sec
-    noTone(buzzer);     // Stop sound...
-    delay(100);        // ...for 1sec
-    }
-    if (isValid = false){
-      tone(buzzer, 1000); // Send 1KHz sound signal...
-      delay(100);        // ...for 1 sec
-      //exit;
-      noTone(buzzer);     // Stop sound...
-    }
-    else {
-      noTone(buzzer);     // Stop sound...
-      }
-}
 
-void close() {
-  for (rotate = 100; rotate >= 1; rotate -= 5) {
-    servoMotor.write(rotate);
-    delay(5);
-  }
-  delay(1000);
-}
+
+
