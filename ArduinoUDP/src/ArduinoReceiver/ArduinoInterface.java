e6bbc753a103c8f5dd0bf3b5ce6871f144d01be4
Christian Fisher <christian.fisher@carleton.ca>
2019-12-05 14:47:32 -0500

    Updated arduino and partially workgin arduino

--- ArduinoUDP/src/ArduinoReceiver/ArduinoInterface.java
+++ ArduinoUDP/src/ArduinoReceiver/ArduinoInterface.java
@@ -10,7 +10,11 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -19,38 +23,70 @@ import java.util.Arrays;
 public class ArduinoInterface {
 
     public static void main(String[] args) {
-        try {
-            SerialPort[] ports = SerialPort.getCommPorts();
-            
-            SerialPort port = ports[0];
 
-            port.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 2000, 2000);
-            port.setBaudRate(9600);
+        SerialPort[] ports = SerialPort.getCommPorts();
+
+        SerialPort port = ports[0];
+        port.openPort();
+        port.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 2000, 0);
+        port.setBaudRate(9600);
+
+        int numRead[] = new int[5];
+        byte[] readBuffer = new byte[6];
+        boolean run = true;
+        String pin = "";
+
+        try {
             DatagramSocket socket = new DatagramSocket(3003);
             DatagramSocket sendSocket = new DatagramSocket();
-            InetAddress Server = InetAddress.getByName("localhost");
-
-           
+            InetAddress ParkingController = InetAddress.getByName("localhost");
             while (true) {
-                byte[] readBuffer = new byte[20];
-                int numofBytesRead = port.readBytes(readBuffer, 4);
-                String pin = new String(readBuffer);
-                System.out.println(pin);
-                if (numofBytesRead!=-1) {
-                    System.out.println(pin);
-                    DatagramPacket pinAck = new DatagramPacket(new byte[100], 100);
-                    sendSocket.send(new DatagramPacket(("ARD:" + new String(readBuffer)).getBytes(), ("ARD:" + new String(readBuffer)).getBytes().length, Server, 2001));
-                    socket.receive(pinAck);
-                    if (new String(pinAck.getData()).trim().split(":")[1].equals("true")) {
-                        port.writeBytes("1".getBytes(), "1".getBytes().length);
-                    } else {
-                        port.writeBytes("0".getBytes(), "0".getBytes().length);
-                    }
+                while (run) {
+                    int x = 0;
+                    readBuffer = new byte[6];
+                    port.readBytes(readBuffer, 2);
+                    if (readBuffer[0] != 0) {
+                        
+//                        System.out.println(Arrays.toString(readBuffer));
+//
+//                        numRead[x] = readBuffer[0];
+//                        if (numRead[x] == 10) {
+//                            run = false;
+//                        }
+//                        x++;
+//                    }
+//                    Thread.sleep(100);
                 }
+                for (int i = 0; i < 4; i++) {
+                    numRead[i] = numRead[i] - 48;
+                    pin += Integer.toString(numRead[i]);
+                }
+//                DatagramPacket ack = new DatagramPacket(new byte[20], 20);
+//                sendSocket.send(new DatagramPacket(pin.getBytes(), pin.getBytes().length, ParkingController, 2001));
+//                socket.receive(ack);
+//                if (new String(ack.getData()).trim().split(":")[1].equals("true")) {
+                    byte response[] = new byte[1];
+                    port.writeBytes(response, 1);
+//                } else {
+//                    byte response[] = new byte[0];
+//                    port.writeBytes(response, 0);
+//                }
+                port.closePort();
+                run =true;
             }
-        } catch (SerialPortInvalidPortException | IOException e) {
+        } catch (SerialPortInvalidPortException e) {
             System.err.println(e);
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(ArduinoInterface.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SocketException ex) {
+            Logger.getLogger(ArduinoInterface.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(ArduinoInterface.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(ArduinoInterface.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-    }
+    }   catch (SocketException ex) {
+            Logger.getLogger(ArduinoInterface.class.getName()).log(Level.SEVERE, null, ex);
+        }
 }
--- arduinoFinal/arduinoFinal.ino
+++ arduinoFinal/arduinoFinal.ino
@@ -1,95 +1,90 @@
 #include <Keypad.h>
-
 #include <Wire.h>
 // Include the Servo Motor library
 #include <Servo.h>
 //Iclude the Keypad library
-//Iclude the LCD library
-Servo servoMotor;
-int IRsensor1 = 0;
-int ledPin = 11; // same as mentioned below
-int signalPin = 12;// need to edit based on the connection for pin code
+Servo servoExit, servoEntry;
+int IRsensor = 13;
 int PIN_CodeLength = 5;
-float sensorRightValue;
-float sensorLeftValue;
-boolean range;
 int rotate;
-int pin = 0;
-int val =0;
+int pin = HIGH;
 const int buzzer = 10; //buzzer to arduino pin 10
 boolean checkingPin = false;
-//Keypad
+boolean pinOK = false;
 
 char pinReceive[4];
-char pinSet[] = "1234";
+String correctPin = "1234";
+String pinEntered = "";
 byte pinReceive_count = 0;
 byte pinSet_count = 0;
 bool checkPin; //checks for correct pin
 char entryKey; // user
 const byte ROWS = 4;
 const byte COLUMNS = 4;
+char pinSet[] = "1234";
 
 char keysPad[ROWS][COLUMNS] = {
   {
-    '1', '2', '3', 'A'  }
+    '1', '2', '3', 'A'
+  }
   ,
   {
-    '4', '5', '6', 'B'  }
+    '4', '5', '6', 'B'
+  }
   ,
   {
-    '7', '8', '9', 'C'  }
+    '7', '8', '9', 'C'
+  }
   ,
   {
-    '*', '0', '#', 'D'  }
+    '*', '0', '#', 'D'
+  }
 };
 
 byte pins1[ROWS] = {
-  9, 8, 7, 6};
+  9, 8, 7, 6
+};
 byte pins2[COLUMNS] = {
-  5, 4, 3, 2};
+  5, 4, 3, 2
+};
 
 Keypad pinKeys = Keypad(makeKeymap(keysPad), pins1, pins2, ROWS, COLUMNS);
 
 void setup() {
   Serial.begin(9600);        //Serial connection initial setup at 9600 bits of data per second
   //pinMode(11, OUTPUT);       //setting the pin 13 in the arduino board as output pin
-  servoMotor.attach(11);      // attaching the servo motor function to pin 9
-  servoMotor.write(0);       // intial set up of the position of the servo motor which is now at 0 degrees
+  servoEntry.attach(12);      // attaching the servo motor function to pin 9
+  servoEntry.write(100);       // intial set up of the position of the servo motor which is now at 0 degrees
+  servoExit.attach(11);      // attaching the servo motor function to pin 9
+  servoExit.write(0);
   pinKeys.begin(makeKeymap(keysPad));
-  pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
-  pinMode(IRsensor1,INPUT);  //setting the first IR sensor as input
+  pinMode(buzzer, OUTPUT); // Set buzzer - pin 10 as an output
+  //pinMode(ledPin, OUTPUT); // Set buzzer - pin 12 as an output
+  pinMode(IRsensor, INPUT); //setting the first IR sensor as input
 
 }
 
+void exitGate() {
+  pin = digitalRead(IRsensor);
+  if (pin == LOW) {
+    for (rotate = 0; rotate <= 100; rotate += 1) { //rotating the motor to 100 degrees to open gate
+      servoExit.write(rotate);
+      delay(15);
+    }
+    delay(5000);
+    for (rotate = 100; rotate >= 1; rotate -= 1) { // closing gate after a delay
+      servoExit.write(rotate);
+      delay(15);
+    }
+    delay(1000);
+  }
+}
 void loop() {
   entry();
+  //buzz(false);
+  //buzz(true);
+  exitGate();
 
-}
-void entry() {
-  char entryKey = pinKeys.getKey();
-  if (entryKey) {
-    Serial.print(entryKey);
-    pinReceive[pinReceive_count] = entryKey;
-    pinReceive_count++;
-  }
-
-  if (pinReceive_count == PIN_CodeLength - 1) {
-    Serial.println(pinReceive);
-
-    if (Serial.available()>=1) {
-      String response = Serial.readString();
-      if(response.equals("1")){
-        buzz(true);
-        open();
-        delay(5000);
-        close();
-      }
-      else{
-        buzz(false);
-      }
-    }
-    clearData();
-  }
 }
 void clearData() {
   while (pinReceive_count != 0) {
@@ -98,50 +93,75 @@ void clearData() {
   return;
 }
 
-void exit(){
+void buzz(boolean isValid) {
 
-  val = digitalRead(IRsensor1);     // read the input pin
-  //Serial.print(val);
-  //digitalWrite(ledPin, val);    // sets the LED to the button's value
-  if ( val ==1){
-    open();
-    delay(2000);
-    close();      
-  }
-
-
-}
-void open() {
-  for (rotate = 0; rotate <= 100; rotate += 1) { //rotating the motor to 90 degrees signifies the open gate
-    servoMotor.write(rotate);
-    delay(15);
-  }
-  delay(1000);
-}
-
-void buzz(bool isValid){
-
-  if (isValid = true){
-    tone(buzzer, 1000); // Send 1KHz sound signal...
-    delay(100);        // ...for 1 sec
+  if (isValid == false) {
+    tone(buzzer, 100); // Send 1KHz sound signal...
+    delay(1000);
     noTone(buzzer);     // Stop sound...
-    delay(100);        // ...for 1sec
+    //delay(100);        // ...for 1sec
   }
-  if (isValid = false){
-    tone(buzzer, 1000); // Send 1KHz sound signal...
+  else if (isValid == true) {
+    tone(buzzer, 100); // Send 1KHz sound signal...
     delay(100);        // ...for 1 sec
-    //exit;
-    noTone(buzzer);     // Stop sound...
+    noTone(buzzer);    // Stop sound...
+    tone(buzzer, 100);
+    noTone(buzzer);
   }
   else {
     noTone(buzzer);     // Stop sound...
   }
 }
-
-void close() {
-  for (rotate = 100; rotate >= 1; rotate -= 5) {
-    servoMotor.write(rotate);
-    delay(5);
+void entryGate(boolean pinOK) {
+  if (pinOK == true) {
+    for (rotate = 100; rotate >= 0; rotate -= 1) { //rotating the motor to 100 degrees to open gate
+      servoEntry.write(rotate);
+      delay(15);
+    }
+    delay(5000);
+    for (rotate = 0; rotate <= 100; rotate += 1) { // close gate after a delay
+      servoEntry.write(rotate);
+      delay(15);
+    }
   }
-  delay(1000);
+
+}
+
+void entry() {
+
+  char entryKey = pinKeys.getKey();
+  if (entryKey) {
+    pinReceive[pinReceive_count] = entryKey;
+    pinReceive_count++;
+  }
+
+  if (pinReceive_count == PIN_CodeLength - 1) {
+//    for (int i = 0; i < 4; i++) {
+//      byte lengthArray[pinReceive[i]] = {0,0,0,0,0,0,0,0,0};
+//          Serial.println((sizeof(lengthArray) / sizeof(lengthArray[0])));
+//          delay(100);
+//    }
+//    byte LengthArray[10] = {0,0,0,0,0,0,0,0,0,0};
+//    Serial.println((sizeof(LengthArray) / sizeof(LengthArray[0])));
+    Serial.println(pinReceive);
+    while (!Serial.available()) {}
+    while(Serial.available()){
+      delay(3);
+      char character = Serial.read();
+      inputString += character;
+    }
+    inputString.trim();
+    if (inputString.length()>0) {
+      buzz(true);
+      delay(1000);
+      buzz('1');
+      entryGate(true);
+    }
+    else {
+      buzz(false);
+    }
+    clearData();
+  }
+
+
 }
